e5489ad1c4e52b8843f2352277269d1b403cb700
m00n <mjm3204@naver.com>
2026-08-06 13:03:20 +0900

    refactor: 띄워쓰기 문단 재조정

--- src/main/java/site/omagotchi/learningservice/ranking/infrastructure/StudyTimeRankingRow.java
+++ src/main/java/site/omagotchi/learningservice/ranking/infrastructure/StudyTimeRankingRow.java
@@ -3,8 +3,6 @@ package site.omagotchi.learningservice.ranking.infrastructure;
 import java.util.UUID;
 
 public interface StudyTimeRankingRow {
-
     UUID getUserId();
-
     long getStudySeconds();
 }

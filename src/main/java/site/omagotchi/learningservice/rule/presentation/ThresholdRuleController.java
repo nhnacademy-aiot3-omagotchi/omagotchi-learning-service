9b4491fe5404a198b3b5550a2fb6cfe23c23c8a0
erase1657 <erase1657@naver.com>
2026-08-11 18:18:50 +0900

    fix: 경로 수정

--- src/main/java/site/omagotchi/learningservice/rule/presentation/ThresholdRuleController.java
+++ src/main/java/site/omagotchi/learningservice/rule/presentation/ThresholdRuleController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/rules")
+@RequestMapping("api/v1/threshold-rules")
 public class ThresholdRuleController {
 
     private final ThresholdRuleService thresholdRuleService;
@@ -38,7 +38,7 @@ public class ThresholdRuleController {
         return new CreateThresholdRuleResponse(ruleId);
     }
 
-    @PatchMapping("/{ruleId}")
+    @PatchMapping("/{rule-id}")
     public UpdateThresholdRuleResponse update(
             @PathVariable Long ruleId,
             @Valid @RequestBody UpdateThresholdRuleRequest request,

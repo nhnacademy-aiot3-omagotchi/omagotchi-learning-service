fe535c84e253e457efa1b43e9dac31e6b10dd841
erase1657 <erase1657@naver.com>
2026-08-11 18:18:11 +0900

    Revert "fix: 경로 수정"
    This reverts commit 510dc792a56b74a2cb5a57bf58dd766021dbd761.

--- src/main/java/site/omagotchi/learningservice/rule/presentation/ThresholdRuleController.java
+++ src/main/java/site/omagotchi/learningservice/rule/presentation/ThresholdRuleController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/threshold-rule")
+@RequestMapping("api/v1/rules")
 public class ThresholdRuleController {
 
     private final ThresholdRuleService thresholdRuleService;
@@ -38,7 +38,7 @@ public class ThresholdRuleController {
         return new CreateThresholdRuleResponse(ruleId);
     }
 
-    @PatchMapping("/{rule-id}")
+    @PatchMapping("/{ruleId}")
     public UpdateThresholdRuleResponse update(
             @PathVariable Long ruleId,
             @Valid @RequestBody UpdateThresholdRuleRequest request,

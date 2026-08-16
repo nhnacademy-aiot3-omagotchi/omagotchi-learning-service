09b025ea1f338478b0192392fc793c28a1e2b5d5
erase1657 <erase1657@naver.com>
2026-08-16 14:09:18 +0900

    fix: 문법 수정

--- src/main/java/site/omagotchi/learningservice/environment/domain/SensorDetection.java
+++ src/main/java/site/omagotchi/learningservice/environment/domain/SensorDetection.java
@@ -19,14 +19,13 @@ public record SensorDetection (
      * compact 생성자로 필드값 검증
      * </p>
      * type에 따라 다른 필드가 null일 수 있어 확정 필드 2개만 검증
-     * TODO: 검증에 대한건 세윤님 파트 확인 필요할듯..
      */
     public SensorDetection{
         if(Objects.isNull(type)){
             throw new IllegalArgumentException("SensorEventType이 null입니다.");
         }
 
-        if(Objects.isNull(receivedAt == null)){
+        if(Objects.isNull(receivedAt)){
             throw new IllegalArgumentException("receivedAt이 null입니다.");
         }
     }

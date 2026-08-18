2ecc92b11f18e5deeefaec29adc43377bb032b22
sy-103 <tpdbs3616@naver.com>
2026-08-18 09:29:29 +0900

    influx 설정

---
+++ src/main/java/site/omagotchi/learningservice/sensor/infrastructure/SensorInfluxProperties.java
@@ -0,0 +1,20 @@
+package site.omagotchi.learningservice.sensor.infrastructure;
+
+import com.influxdb.client.domain.Buckets;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "sensor.influx")
+public record SensorInfluxProperties(
+        String url,
+        String token,
+        String org,
+        Buckets buckets
+) {
+    public record Buckets(
+            String raw,
+            String avg1h,
+            String avg1d
+    ){
+
+    }
+}

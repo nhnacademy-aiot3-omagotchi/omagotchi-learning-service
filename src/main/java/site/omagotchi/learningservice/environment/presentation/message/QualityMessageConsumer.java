828ed2ebf7ede2fc5b768453c96fe5003de082f4
erase1657 <erase1657@naver.com>
2026-08-20 10:04:19 +0900

    feat: 품질 메세지 컨슈머 수정

--- src/main/java/site/omagotchi/learningservice/environment/presentation/message/QualityMessageConsumer.java
+++ src/main/java/site/omagotchi/learningservice/environment/presentation/message/QualityMessageConsumer.java
@@ -1,22 +1,21 @@
 package site.omagotchi.learningservice.environment.presentation.message;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+import site.omagotchi.learningservice.environment.application.SensorEventRecordService;
 
 /** RabbitMQ 메세지 진입점- 품질 메세지를 소비함.*/
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class QualityMessageConsumer {
     private static final String CONSUME_QUEUE = "omagotchi.sensor.quality.queue";
+    private final SensorEventRecordService recordService;
 
     @RabbitListener(queues = CONSUME_QUEUE)
-    public void consume(QualityEvent qualityEvent, @Header(name = "traceId", required = false) String traceId){
-        if(qualityEvent.type() == QualityEvent.Type.RULE_HIT){
-            log.info("룰 히트 데이터 발생");
-        }else{
-            log.info("{} 데이터 발생", qualityEvent.type().name());
-        }
+    public void consume(QualityEventMessage message){
+        recordService.record(message.toSensorEvent());
     }
 }

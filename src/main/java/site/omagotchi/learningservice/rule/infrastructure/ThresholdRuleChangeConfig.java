b97d98dce028eed44533472333a16d1c142d6941
erase1657 <erase1657@naver.com>
2026-08-20 15:23:32 +0900

    fix: configuration 추가

--- src/main/java/site/omagotchi/learningservice/rule/infrastructure/ThresholdRuleChangeConfig.java
+++ src/main/java/site/omagotchi/learningservice/rule/infrastructure/ThresholdRuleChangeConfig.java
@@ -3,7 +3,9 @@ package site.omagotchi.learningservice.rule.infrastructure;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ThresholdRuleChangeConfig {
     public static final String EXCHANGE_RULE_CHANGED = "omagotchi.rule.changed.exchange";
 

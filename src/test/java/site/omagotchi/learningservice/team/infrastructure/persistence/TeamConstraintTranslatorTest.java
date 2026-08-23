f9c791c5e3511b0351ff53b9041817c9af0917a7
Quasimorphism <bak575755@gmail.com>
2026-08-23 22:02:29 +0900

    refactor: 팀 영속성 어댑터 패키지 분리

--- src/test/java/site/omagotchi/learningservice/team/infrastructure/persistence/TeamConstraintTranslatorTest.java
+++ src/test/java/site/omagotchi/learningservice/team/infrastructure/persistence/TeamConstraintTranslatorTest.java
@@ -1,4 +1,4 @@
-package site.omagotchi.learningservice.team.infrastructure;
+package site.omagotchi.learningservice.team.infrastructure.persistence;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.DisplayName;

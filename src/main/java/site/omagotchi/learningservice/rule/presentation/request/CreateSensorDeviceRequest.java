3bd44870cc725a9de3af94b3cd4543697f209aca
erase1657 <erase1657@naver.com>
2026-08-24 11:14:50 +0900

    feat: valid 빼먹은거 추가

--- src/main/java/site/omagotchi/learningservice/rule/presentation/request/CreateSensorDeviceRequest.java
+++ src/main/java/site/omagotchi/learningservice/rule/presentation/request/CreateSensorDeviceRequest.java
@@ -1,16 +1,36 @@
 package site.omagotchi.learningservice.rule.presentation.request;
 
+import jakarta.validation.constraints.*;
 import site.omagotchi.learningservice.rule.application.command.CreateSensorDeviceCommand;
 
 import java.time.Instant;
 
 public record CreateSensorDeviceRequest(
+        @NotBlank
+        @Pattern(
+                regexp = "[0-9a-fA-F]+",
+                message = "장치 EUI는 16진수여야 합니다."
+        )
+        @Size(max = 32)
         String deviceEui,
+
         Long spaceId,
+
+        @NotBlank
+        @Size(max = 32)
         String model,
+
+        @NotBlank
+        @Size(max = 64)
         String displayName,
+
+        @Size(max = 64)
         String installationPoint,
+
+        @NotNull
+        @Positive
         Integer expectedIntervalSeconds,
+
         Instant installedAt
 ) {
 

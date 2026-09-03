6446c28af4157e88fdf97838a1bab27d4992595d
erase1657 <erase1657@naver.com>
2026-09-03 10:16:53 +0900

    fix: flyway 버전 수정

--- src/main/resources/db/migration/V21__scope_community_posts_to_cohort.sql
+++ src/main/resources/db/migration/V21__scope_community_posts_to_cohort.sql
@@ -1,11 +1,13 @@
 -- 커뮤니티 게시판을 기수 소속으로 확정한다.
-ALTER TABLE learning_service.community_posts
-    DROP COLUMN scope,
-    ALTER COLUMN cohort_id SET NOT NULL;
-
+-- scope를 먼저 지우면 scope를 포함한 인덱스도 함께 자동 삭제되므로,
+-- 인덱스를 먼저 명시적으로 내린다.
 DROP INDEX learning_service.ix_community_posts_visible_list;
 
 DROP INDEX learning_service.ix_community_posts_search;
+
+ALTER TABLE learning_service.community_posts
+    DROP COLUMN scope,
+    ALTER COLUMN cohort_id SET NOT NULL;
 
 CREATE INDEX ix_community_posts_cohort_list
     ON learning_service.community_posts (
